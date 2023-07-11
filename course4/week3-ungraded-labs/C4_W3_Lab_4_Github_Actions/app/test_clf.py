2b53baf2bc38fee6ef0daf4ef5a18ce68c127303
Maciej Wojciechowski <maciej.wojciechowski@b-yond.com>
2023-07-11 15:19:27 +0200

    test: add unit test

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,9 +1,22 @@
 import pickle
+
 from main import clf
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+
+def test_pipeline_and_scaler():
+    # Check if clf is an instance of Pipeline
+    is_pipeline = isinstance(clf, Pipeline)
+    assert is_pipeline
+
+    if is_pipeline:
+        # Check if the first step is StandardScaler
+        first_step = [i for i in clf.named_steps.values()][0]
+        assert isinstance(first_step, StandardScaler)
 
 
 def test_accuracy():
-
     # Load test data
     with open("data/test_data.pkl", "rb") as file:
         test_data = pickle.load(file)
